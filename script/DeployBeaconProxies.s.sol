de83481a18c26d4f9db50f58777c2b60f719de85
Bobby Fiando <bobfiando@gmail.com>
2025-04-27 23:59:54 +0700

    Fix deploy beacon proxies

--- script/DeployBeaconProxies.s.sol
+++ script/DeployBeaconProxies.s.sol
@@ -1,17 +1,20 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.26;
 
-import "forge-std/Script.sol";
-import "@openzeppelin/contracts/proxy/beacon/UpgradeableBeacon.sol";
-import "@openzeppelin/contracts/proxy/beacon/BeaconProxy.sol";
-import "../src/GTXRouter.sol";
 import "../src/BalanceManager.sol";
-import "../src/PoolManager.sol";
+import "../src/GTXRouter.sol";
+
 import "../src/OrderBook.sol";
-import {Upgrades} from "openzeppelin-foundry-upgrades/Upgrades.sol";
-import {Currency} from "../src/types/Currency.sol";
-import {PoolKey} from "../src/types/Pool.sol";
+import "../src/PoolManager.sol";
+
 import {IOrderBook} from "../src/interfaces/IOrderBook.sol";
+import {Currency} from "../src/libraries/Currency.sol";
+import {PoolKey} from "../src/libraries/Pool.sol";
+import "@openzeppelin/contracts/proxy/beacon/BeaconProxy.sol";
+import "@openzeppelin/contracts/proxy/beacon/UpgradeableBeacon.sol";
+import "forge-std/Script.sol";
+
+import {Upgrades} from "openzeppelin-foundry-upgrades/Upgrades.sol";
 
 contract DeployBeaconProxies is Script {
     function run() public {
@@ -21,25 +24,10 @@ contract DeployBeaconProxies is Script {
 
         vm.startBroadcast(deployerPrivateKey);
 
-        address balanceManagerBeacon = Upgrades.deployBeacon(
-            "BalanceManager.sol",
-            beaconOwner
-        );
-
-        address poolManagerBeacon = Upgrades.deployBeacon(
-            "PoolManager.sol",
-            beaconOwner
-        );
-
-        address routerBeacon = Upgrades.deployBeacon(
-            "GTXRouter.sol",
-            beaconOwner
-        );
-
-        address orderBookBeacon = Upgrades.deployBeacon(
-            "OrderBook.sol",
-            beaconOwner
-        );
+        address balanceManagerBeacon = Upgrades.deployBeacon("BalanceManager.sol", beaconOwner);
+        address poolManagerBeacon = Upgrades.deployBeacon("PoolManager.sol", beaconOwner);
+        address routerBeacon = Upgrades.deployBeacon("GTXRouter.sol", beaconOwner);
+        address orderBookBeacon = Upgrades.deployBeacon("OrderBook.sol", beaconOwner);
 
         console.log("BalanceManager Beacon deployed at:", balanceManagerBeacon);
         console.log("PoolManager Beacon deployed at:", poolManagerBeacon);
@@ -58,19 +46,12 @@ contract DeployBeaconProxies is Script {
 
         address poolManagerProxy = Upgrades.deployBeaconProxy(
             poolManagerBeacon,
-            abi.encodeCall(
-                PoolManager.initialize,
-                (beaconOwner, balanceManagerProxy, orderBookBeacon)
-            )
+            abi.encodeCall(PoolManager.initialize, (beaconOwner, balanceManagerProxy, orderBookBeacon))
         );
         console.log("PoolManager Proxy deployed at:", poolManagerProxy);
 
         address routerProxy = Upgrades.deployBeaconProxy(
-            routerBeacon,
-            abi.encodeCall(
-                GTXRouter.initialize,
-                (poolManagerProxy, balanceManagerProxy)
-            )
+            routerBeacon, abi.encodeCall(GTXRouter.initialize, (poolManagerProxy, balanceManagerProxy))
         );
         console.log("Router Proxy deployed at:", routerProxy);
 
@@ -78,11 +59,9 @@ contract DeployBeaconProxies is Script {
         BalanceManager balanceManager = BalanceManager(balanceManagerProxy);
         PoolManager poolManager = PoolManager(poolManagerProxy);
 
-        // Set router in the pool manager
-        poolManager.setRouter(routerProxy);
-
-        // Authorize the router in the balance manager
-        balanceManager.setAuthorizedOperator(routerProxy, true);
+        balanceManager.setAuthorizedOperator(address(poolManager), true);
+        balanceManager.transferOwnership(address(poolManager));
+        poolManager.setRouter(address(routerProxy));
 
         vm.stopBroadcast();
     }
