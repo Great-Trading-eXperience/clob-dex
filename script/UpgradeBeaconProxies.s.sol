0ccb306035cc78ed0fcb27f66ef16eb80f18c5ce
Bobby Fiando <bobfiando@gmail.com>
2025-05-04 19:20:30 +0700

    Add BEACON_ORDERBOOK address and refactor UpgradeBeaconProxies contract for upgrades

--- script/UpgradeBeaconProxies.s.sol
+++ script/UpgradeBeaconProxies.s.sol
@@ -1,65 +1,63 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.26;
 
+import "./DeployHelpers.s.sol";
 import "forge-std/Script.sol";
 import {Upgrades} from "openzeppelin-foundry-upgrades/Upgrades.sol";
 
-contract UpgradeBeaconProxies is Script {
+contract UpgradeBeaconProxies is DeployHelpers {
     function run() public {
+        loadDeployments();
         uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
-        address beaconOwner = vm.envAddress("BEACON_OWNER_ADDRESS");
-
-        // The addresses of the deployed beacons
-        address balanceManagerBeacon = vm.envAddress("BEACON_BALANCEMANAGER");
-        address poolManagerBeacon = vm.envAddress("BEACON_POOLMANAGER");
-        address routerBeacon = vm.envAddress("BEACON_ROUTER");
-        address orderBookBeacon = vm.envAddress("BEACON_ORDERBOOK");
-
-        // The addresses of the proxy instances
-        address balanceManagerProxy = vm.envAddress("PROXY_BALANCEMANAGER");
-        address poolManagerProxy = vm.envAddress("PROXY_POOLMANAGER");
-        address routerProxy = vm.envAddress("PROXY_ROUTER");
 
         vm.startBroadcast(deployerPrivateKey);
 
-        console.log("========== UPGRADING BEACON IMPLEMENTATIONS ==========");
-        // Upgrade the implementations
-        Upgrades.upgradeBeacon(balanceManagerBeacon, "BalanceManagerV2.sol");
-        console.log("Upgraded BalanceManager beacon to V2");
-
-        Upgrades.upgradeBeacon(poolManagerBeacon, "PoolManagerV2.sol");
-        console.log("Upgraded PoolManager beacon to V2");
-
-        Upgrades.upgradeBeacon(routerBeacon, "GTXRouterV2.sol");
-        console.log("Upgraded GTXRouter beacon to V2");
-
-        Upgrades.upgradeBeacon(orderBookBeacon, "OrderBookV2.sol");
-        console.log("Upgraded OrderBook beacon to V2");
-
-        console.log("\n========== VERIFYING UPGRADES ==========");
-        // Verify upgrades by calling the new getVersion function on each proxy
-        string memory balanceVersion = _callGetVersion(balanceManagerProxy);
-        string memory poolVersion = _callGetVersion(poolManagerProxy);
-        string memory routerVersion = _callGetVersion(routerProxy);
-
-        console.log("BalanceManager version: %s", balanceVersion);
-        console.log("PoolManager version: %s", poolVersion);
-        console.log("GTXRouter version: %s", routerVersion);
+        // Uncomment the functions you want to execute
+        upgradeOrderBook();
+        // upgradeBalanceManager();
+        // upgradePoolManager();
+        // upgradeRouter();
 
         vm.stopBroadcast();
     }
 
-    function _callGetVersion(
-        address proxy
-    ) internal view returns (string memory) {
-        bytes4 selector = bytes4(keccak256("getVersion()"));
-
-        (bool success, bytes memory data) = proxy.staticcall(abi.encodeWithSelector(selector));
-
-        if (!success) {
-            return "Failed to get version";
+    function upgradeOrderBook() internal {
+        address orderBookBeacon = deployed["BEACON_ORDERBOOK"].addr;
+        if (!deployed["BEACON_ORDERBOOK"].isSet) {
+            revert("OrderBook beacon address not found in deployments");
         }
 
-        return abi.decode(data, (string));
+        Upgrades.upgradeBeacon(orderBookBeacon, "OrderBookV2.sol");
+        console.log("Upgraded OrderBook beacon to V2");
+    }
+
+    function upgradeBalanceManager() internal {
+        address balanceManagerBeacon = deployed["BEACON_BALANCEMANAGER"].addr;
+        if (!deployed["BEACON_BALANCEMANAGER"].isSet) {
+            revert("BalanceManager beacon address not found in deployments");
+        }
+
+        Upgrades.upgradeBeacon(balanceManagerBeacon, "BalanceManagerV2.sol");
+        console.log("Upgraded BalanceManager beacon to V2");
+    }
+
+    function upgradePoolManager() internal {
+        address poolManagerBeacon = deployed["BEACON_POOLMANAGER"].addr;
+        if (!deployed["BEACON_POOLMANAGER"].isSet) {
+            revert("PoolManager beacon address not found in deployments");
+        }
+
+        Upgrades.upgradeBeacon(poolManagerBeacon, "PoolManagerV2.sol");
+        console.log("Upgraded PoolManager beacon to V2");
+    }
+
+    function upgradeRouter() internal {
+        address routerBeacon = deployed["BEACON_ROUTER"].addr;
+        if (!deployed["BEACON_ROUTER"].isSet) {
+            revert("Router beacon address not found in deployments");
+        }
+
+        Upgrades.upgradeBeacon(routerBeacon, "GTXRouterV2.sol");
+        console.log("Upgraded Router beacon to V2");
     }
 }
